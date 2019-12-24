363a41cc8a0d32e61b82f3874d900c1ea9e20a22
tomaslingotti <tomaslingotti@gmail.com>
2019-12-24 03:21:29 -0300

    add transaction to persist a smartphone

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,1 +1,13 @@
 package main
+
+import (
+	"github.com/tomiok/course-phones-review/internal/database"
+	"github.com/tomiok/course-phones-review/internal/logs"
+)
+
+func main() {
+	_ = logs.InitLogger()
+
+	client := database.NewSqlClient("root:root@/phones-review")
+
+}
